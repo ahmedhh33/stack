a1e293deb12199a2d29a17cd5f35d746e7ea5a4d
ahmedhh33 <ahmedhh3633@gmail.com>
2023-07-18 22:11:58 +0400

    stack

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack mystack = new Stack(5);
+            mystack.Push(1);
+            mystack.Push(2);
+            mystack.Push(3);
+            mystack.Push(4);
+            mystack.Push(5);
+            mystack.Push(6);//it dos not pushed 
+            Console.WriteLine(mystack.Pop());//delet the last pushed one 5
+            Console.WriteLine(mystack.peak());//shows the top element 4
+            Console.WriteLine(mystack.Pop());//delet 4
+            mystack.Push(6);//add 6
+            Console.WriteLine(mystack.peak());//shows 6
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack
+{
+    internal class Stack
+    {
+        private int[] stack;
+        private int Top;
+
+        public Stack(int n)
+        {
+            stack = new int[n];
+            Top = -1;
+        }
+
+        public void Push(int number)
+        {
+            if(Top >= stack.Length-1)
+            {
+                Console.WriteLine("The stack is full");
+                return;//to avoid ex
+            }
+            //Top++ same as ex[resstio bellow
+            stack[++Top] = number;
+
+        }
+        public int Pop()
+        {
+            if (Top == -1)
+            {
+                Console.WriteLine("stack is empty");
+                return -1;
+            }
+            return stack[Top--];
+        }
+
+        public int peak()
+        {
+            if (Top == -1)
+            {
+                Console.WriteLine("stack is empty");
+            }
+            return stack[Top];
+        }
+    }
+}
